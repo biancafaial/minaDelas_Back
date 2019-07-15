9d9ed2545329d62f31c890d14f133afa25ca36d6
biancafaial <biancafaial@gmail.com>
2019-07-14 23:36:03 -0300

    add mongodb url

--- MinasRepository.js
+++ MinasRepository.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 // string de conexão:
 // mongodb://dominio:porta/nome_database
-const MONGO_URL = "mongodb://localhost:27017/minasApi";
+// const MONGO_URL = "mongodb://localhost:27017/minasApi";
+const MONGO_URL = "mongodb://heroku_j18sdc8d:h1dodm7fi377sk3ho7hvvgjl6c@ds133187.mlab.com:33187/heroku_j18sdc8d"
 
 function connect () {
   mongoose.connect(MONGO_URL,
